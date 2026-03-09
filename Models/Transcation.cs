e3a36e2a640fb3cbc3bc1df57777794fbf11612a
Coppermine-SP <son5121502@gmail.com>
2026-03-09 17:20:17 +0900

    Add project files.

---
+++ Models/Transcation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace coffeetime.Models
+{
+    public class Transcation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransactionId { get; set; }
+
+        public required virtual PackageBatch Inventory { get; set; }
+
+        [Required]
+        public required string Subject { get; set; }
+
+        [Range(1, 10)]
+        public required uint Amount { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
