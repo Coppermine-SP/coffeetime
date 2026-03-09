8d423debfb6a9e879dd809309349bcef20068a58
Coppermine-SP <son5121502@gmail.com>
2026-03-09 17:20:17 +0900

    init: ASP.NET Blazor Project, Data Model

---
+++ Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace coffeetime.Models
+{
+    public class Item
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ItemId { get; set; }
+
+        [Required]
+        public required string ItemName { get; set; }
+
+        [Required]
+        public string ItemDescription { get; set; } = string.Empty;
+
+        [Required]
+        public uint ItemPrice { get; set; }
+
+        public virtual List<PackageBatch> Batches { get; set; } = [];
+    }
+}
---
+++ Models/PackageBatch.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace coffeetime.Models
+{
+    public class PackageBatch
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BatchId { get; set; }
+
+        public required virtual Item Item { get; set; }
+
+        [Required]
+        public DateTime RoastedAt { get; set; } = DateTime.Now;
+
+        [Required]
+        [Range(1, 30)]
+        public required int BatchCount { get; set; }
+
+        public virtual List<Transcation> Transcations { get; set; } = [];
+    }
+}
