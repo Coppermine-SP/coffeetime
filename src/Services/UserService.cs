d3d4e487e66a74c5bd515127c6fac56dcc6de589
Coppermine-SP <son5121502@gmail.com>
2026-03-13 10:54:07 +0900

    feat: User displayName 메모리 캐시, 조회, 업데이트

---
+++ src/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using coffeetime.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace coffeetime.Services
+{
+    public class UserService(
+        IDbContextFactory<ServerDbContext> factory,
+        ILogger<UserService> logger,
+        IMemoryCache cache)
+    {
+        public async Task CreateOrUpdateUserCacheAsync(string userObjectGuid, string userDisplayName)
+        {
+            using var context = await factory.CreateDbContextAsync();
+
+            logger.LogInformation($"Update UserCache {userObjectGuid} => {userDisplayName}");
+            int rows = await context.UserCaches.
+                Where(u => u.UserObjectGuid == userObjectGuid).
+                ExecuteUpdateAsync(u => u.SetProperty(uc => uc.UserDisplayName, userDisplayName));
+
+            if (rows == 0)
+            {
+                logger.LogInformation($"Create UserCache {userObjectGuid} => {userDisplayName}");
+                await context.UserCaches.AddAsync(new Models.UserCache
+                {
+                    UserObjectGuid = userObjectGuid,
+                    UserDisplayName = userDisplayName
+                });
+
+                await context.SaveChangesAsync();
+            }
+
+            cache.Set(userObjectGuid, userDisplayName, TimeSpan.FromHours(8));
+        }
+
+        public async Task<string> GetUserDisplayName(string userObjectGuid)
+        {
+            if (cache.TryGetValue(userObjectGuid, out string? userDisplayName))
+            {
+                logger.LogInformation($"Found UserCache from in-memory cache {userObjectGuid} => {userDisplayName}");
+                return userDisplayName!;
+            }
+            using var context = await factory.CreateDbContextAsync();
+
+            return (await context.UserCaches.
+                AsNoTracking().
+                FirstAsync(
+                x => x.UserObjectGuid == userObjectGuid)).UserDisplayName;
+        }
+    }
+}
